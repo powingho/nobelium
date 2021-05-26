f4cb47eb50b55593f9d7ceaeedc3b76023871645
powingho <nanagubenzim@yahoo.com>
2021-05-27 05:56:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  socialLink: 'https://twitter.com/keonandc',
+  socialLink: 'https://space.bilibili.com/9337102',
   seo: {
     keywords: ['少女时代', '翻译', '中字', '权俞利', '权侑莉', '徐贤', '徐珠贤', '崔秀英', '林允儿', '林润娥', '崔秀英', '黄美英', 'Tiffany Young'],
     googleSiteVerification: 'UA-145324150-1' // Remove the value or replace it with your own google site verification code
