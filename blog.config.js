be7a0fc71973ebf36ca7d6bdf4fbaa756be97d23
powingho <nanagubenzim@yahoo.com>
2021-05-27 13:08:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -31,7 +31,7 @@ const BLOG = {
       domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
     gaConfig: {
-      measurementId: 'G-8KDGEMLTT8' // e.g: G-XXXXXXXXXX
+      measurementId: 'G-ZD5MEW2BSH' // e.g: G-XXXXXXXXXX
     }
   },
   comment: {
