83f2697962cff188edee34dcdddb2b777a80e03c
powingho <nanagubenzim@yahoo.com>
2021-08-19 03:44:04 +0800

    update ogImageGenerateURL

--- components/Container.js
+++ components/Container.js
@@ -40,7 +40,7 @@ const Container = ({ children, layout, fullWidth, ...customMeta }) => {
           property="og:image"
           content={`${BLOG.ogImageGenerateURL}/${encodeURIComponent(
             meta.title
-          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
+          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg&images=https%3A%2F%2Fher.hercoffee.tk%2Ffavicon.svg`}
         />
         <meta property="og:type" content={meta.type} />
         <meta name="twitter:card" content="summary_large_image" />
